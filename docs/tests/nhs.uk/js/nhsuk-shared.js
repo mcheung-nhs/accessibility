0b673223e9e95cbff5dca1e7c26492a41cbc2ef3
Michael Cheung <michaelcheung@nhs.net>
2018-09-07 14:46:12 +0100

    :link: Testing the skip link

--- docs/tests/nhs.uk/js/nhsuk-shared.js
+++ docs/tests/nhs.uk/js/nhsuk-shared.js
@@ -13288,8 +13288,8 @@ if (!Array.prototype.fill) {
 
 ;$( document ).ready(function() {
   $(".nhsuk-c-skiplink__link").click(function(event){
-    var skipTo="#"+this.href.split('#')[1];
-    $(skipTo).attr('tabindex', -1).on('blur focusout', function () {
+    //var skipTo="#"+this.href.split('#')[1];
+    $(':header:first').attr('tabindex', -1).on('blur focusout', function () {
       $(this).removeAttr('tabindex');
     }).focus();
   });
